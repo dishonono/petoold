531da974a3f197d1faa5cf6e403137fc45d2651d
Noam Dishon <noam.dishon@wework.com>
2017-08-23 17:37:33 +0300

    change root and add link to genres

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants
   resources :genres
+  root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
